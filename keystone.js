f837aa3cc2f24a2f427978599b968229c1a70978
martin <mvarmuza@gmail.com>
2018-08-20 22:35:36 +0200

    to the moon

--- keystone.js
+++ keystone.js
@@ -11,29 +11,31 @@ var handlebars = require('express-handlebars');
 // and documentation.
 
 keystone.init({
-	'name': 'MUDr. Helena Filová',
-	'brand': 'MUDr. Helena Filová',
+		'name': 'MUDr. Helena Filová',
+		'brand': 'MUDr. Helena Filová',
 
-	'less': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': '.hbs',
+		'less': 'public',
+		'static': 'public',
+		'favicon': 'public/favicon.ico',
+		'views': 'templates/views',
+		'view engine': '.hbs',
 
-	'custom engine': handlebars.create({
-		layoutsDir: 'templates/views/layouts',
-		partialsDir: 'templates/views/partials',
-		defaultLayout: 'default',
-		helpers: new require('./templates/views/helpers')(),
-		extname: '.hbs',
-	}).engine,
+		'custom engine': handlebars.create({
+				layoutsDir: 'templates/views/layouts',
+				partialsDir: 'templates/views/partials',
+				defaultLayout: 'default',
+				helpers: new require('./templates/views/helpers')(),
+				extname: '.hbs',
+		}).engine,
 
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
+		'auto update': true,
+		'session': true,
+		'auth': true,
+		'user model': 'User',
+		'mongo': process.env.MONGODB_URI,
 
-	'cookie secret': '2ea39d49359ed81f76ac22640af9b6e52c3d1bf3c6bd9728b8a01b16e270021f4178ea474ee53358099b8b926880836c7e60648c75d287648b2366d6fcb3a549'
+
+		'cookie secret': '2ea39d49359ed81f76ac22640af9b6e52c3d1bf3c6bd9728b8a01b16e270021f4178ea474ee53358099b8b926880836c7e60648c75d287648b2366d6fcb3a549'
 });
 
 keystone.set('cloudinary config', { cloud_name: 'janosch', api_key: '374969271443423', api_secret: 'HgAtXVTcKwvIclAaCEblyFMHMJM' });
@@ -46,10 +48,10 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
+		_: require('lodash'),
+		env: keystone.get('env'),
+		utils: keystone.utils,
+		editable: keystone.content.editable,
 });
 
 // Load your project's Routes
@@ -58,14 +60,12 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	Ordinace: 'ordinaces',
-	Zprávy: 'enquiries',
-	Blog: ['posts'],
-	Správci: 'users',
+		Ordinace: 'ordinaces',
+		Zprávy: 'enquiries',
+		Blog: ['posts'],
+		Správci: 'users',
 });
 
 // Start Keystone to connect to your database and initialise the web server
-
-
 
 keystone.start();
